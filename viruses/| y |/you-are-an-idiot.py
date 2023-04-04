8cba2c2a2d6546f8ac20a449e5af08b550047644
khr1st <120200684+krisctf@users.noreply.github.com>
2023-04-04 17:24:56 -0500

    Update and rename test to you-are-an-idiot.py

---
+++ viruses/| y |/you-are-an-idiot.py
@@ -0,0 +1,19 @@
+# =======================================================================>
+# You are an idiot by khr1st
+# 
+# no further explanation needed, you are an idiot!
+# =======================================================================>
+
+# This virus is intended for educational purposes only and should not be used for any malicious activities.
+
+import sys
+
+def display_warning():
+    print("You are an idiot! Ha Ha Ha!")
+    # Additional code here to display insulting messages or perform malicious actions.
+
+def main():
+    display_warning()
+
+if __name__ == '__main__':
+    main()
